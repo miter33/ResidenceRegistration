69fca04c57441aef84ef4c99e545e83cb809f59f
Aliaksei Huivan <a.huivan@godeltech.com>
2022-12-26 00:13:59 +0100

    make app more efficient

--- ResidenceRegistration/Program.cs
+++ ResidenceRegistration/Program.cs
@@ -13,7 +13,7 @@ namespace ResidenceRegistration
     internal static class Program
     {
         private static readonly HttpClient httpClient = new HttpClient();
-        private const string LAST_DATE = "2023-01-10";
+        private const string LAST_DATE = "2023-01-16";
         private static string[] KEY_WORDS =
         {
             "Identyfikator wizyty",
@@ -33,41 +33,47 @@ namespace ResidenceRegistration
             {
                 new UserData { Email = "alexg9000000@gmail.com", VerificationNumber = 6498 },
                 new UserData { Email = "alexg9000000@gmail.com", VerificationNumber = 6498 },
-                new UserData { Email = "aliaksei.huivan@gmail.com", VerificationNumber = 6498 },
-                new UserData { Email = "aliaksei.huivan@gmail.com", VerificationNumber = 6498 },
+                new UserData { Email = "alexg9000000@gmail.com", VerificationNumber = 6498 },
+                new UserData { Email = "alexg9000000@gmail.com", VerificationNumber = 6498 },
+                //new UserData { Email = "aliaksei.huivan@gmail.com", VerificationNumber = 6498 },
                 //new UserData { Email = "miter30@mail.ru", VerificationNumber = 6498 },
             };
 
+
             while (true)
             {
-                var tasks = users.Select(user => GenerateFormData(user));
+                List<Task> tasks = new();
+                List<string> selectedTimes = new();
+
+                var timeUrlReceiver = $"https://rezerwacje.bialystok.uw.gov.pl/?godziny=1&branch=183&service=201&data={LAST_DATE}";
+
+                var timeHttpResult = await httpClient.GetAsync(timeUrlReceiver);
+                var timeResponse = await timeHttpResult.Content.ReadAsStringAsync();
+
+                var timeStr = timeResponse.Substring(1, timeResponse.Length - 2);
+
+                if (string.IsNullOrEmpty(timeStr)) continue;
+
+                var timeArray = GetSplittedStringContent(timeStr);
+
+                Random random = new();
+
+                foreach (var user in users)
+                {
+                    timeArray = timeArray.Where(_ => !selectedTimes.Contains(_)).ToArray();
+
+                    var randomTime = timeArray[random.Next(timeArray.Length)];
+                    
+                    if (!TimeOnly.TryParse(randomTime, out TimeOnly time)) continue;
+
+                    selectedTimes.Add(randomTime);
+                    tasks.Add(SubmitForm(randomTime, LAST_DATE, user.Email, user.VerificationNumber));
+                }
+
+                DisplayInfo(timeStr, DayParam.Times);
 
                 await Task.WhenAll(tasks);
             }
-        }
-
-        private async static Task GenerateFormData(UserData user)
-        {
-            var timeUrlReceiver = $"https://rezerwacje.bialystok.uw.gov.pl/?godziny=1&branch=183&service=201&data={LAST_DATE}";
-
-            var timeHttpResult = await httpClient.GetAsync(timeUrlReceiver);
-            var timeResponse = await timeHttpResult.Content.ReadAsStringAsync();
-
-            var timeStr = timeResponse.Substring(1, timeResponse.Length - 2);
-
-            if (string.IsNullOrEmpty(timeStr)) return;
-
-            var timeArray = GetSplittedStringContent(timeStr);
-
-            Random random = new();
-
-            var randomTime = timeArray[random.Next(timeArray.Length)];
-
-            if (!TimeOnly.TryParse(randomTime, out TimeOnly time)) return;
-
-            DisplayInfo(timeStr, DayParam.Times);
-
-            await SubmitForm(randomTime, LAST_DATE, user.Email, user.VerificationNumber);
         }
 
         private async static Task SubmitForm(string time, string date, string email, int verification)
@@ -87,22 +93,54 @@ namespace ResidenceRegistration
 
             if (httpClientResponse.IsSuccessStatusCode && (KEY_WORDS.Any(key => result.Contains(key, StringComparison.OrdinalIgnoreCase))))
             {
-                Console.BackgroundColor = ConsoleColor.DarkGreen;
-                Console.WriteLine($"POSTED DATA (date: {date}, time: {time}, email: {email}, verification: {verification}):\n{body}");
-
-                Console.ResetColor();
-
-                await WriteToFile(email, date, time, result, "SUCCESS");
+                await LogResponse(RegistrationStatus.Success, time, date, email, verification, result, body);
+            }
+            else if (result.Contains("Termin niedostępny", StringComparison.OrdinalIgnoreCase))
+            {
+                await LogResponse(RegistrationStatus.TimeIsUnavailable, time, date, email, verification, result);
+            }
+            else if (result.Contains("Blad serwera", StringComparison.OrdinalIgnoreCase))
+            {
+                await LogResponse(RegistrationStatus.ServerError, time, date, email, verification, result);
+            }
+            else if (result.Contains("You have exceeded the maximum", StringComparison.OrdinalIgnoreCase))
+            {
+                await LogResponse(RegistrationStatus.Exceeding, time, date, email, verification, result);
             }
             else
             {
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.WriteLine($"SUBMIT FORM IS FAILED (date: {date}, time: {time}, email: {email}, verification: {verification})");
-
-                Console.ResetColor();
-
-                await WriteToFile(email, date, time, result, "FAILED");
+                await LogResponse(RegistrationStatus.Failed, time, date, email, verification, result);
             }
+        }
+
+        private static async Task LogResponse(RegistrationStatus status, string time, string date, string email, int verification, string result, string body = null)
+        {
+            Console.BackgroundColor = status == RegistrationStatus.Success ? ConsoleColor.DarkGreen : ConsoleColor.Red;
+            switch (status)
+            {
+                case RegistrationStatus.Success:
+                    Console.WriteLine($"SUCCESS: (date: {date}, time: {time}, email: {email}, verification: {verification}):\n{body}");
+                    break;
+                case RegistrationStatus.TimeIsUnavailable:
+                    Console.WriteLine($"TERMIN NIEDOSTUPNE: (date: {date}, time: {time}, email: {email}, verification: {verification})");
+                    break;
+                case RegistrationStatus.ServerError:
+                    Console.WriteLine($"BLAD SERWERA: (date: {date}, time: {time}, email: {email}, verification: {verification})");
+                    break;
+                case RegistrationStatus.Exceeding:
+                    Console.WriteLine($"EXCEEDING: (date: {date}, time: {time}, email: {email}, verification: {verification})");
+                    break;
+                case RegistrationStatus.Failed:
+                    Console.WriteLine($"FAILED: (date: {date}, time: {time}, email: {email}, verification: {verification})");
+                    break;
+                default:
+                    Console.WriteLine($"FAILED: (date: {date}, time: {time}, email: {email}, verification: {verification})");
+                    break;
+            }
+
+            Console.ResetColor();
+
+            await WriteToFile(email, date, time, result, status.ToString().ToUpper());
         }
 
         private static async Task WriteToFile(string email, string applyingDate, string applyingTime, string result, string status)
@@ -146,5 +184,14 @@ namespace ResidenceRegistration
     {
         Dates,
         Times
+    }
+
+    enum RegistrationStatus
+    {
+        Success,
+        TimeIsUnavailable,
+        ServerError,
+        Exceeding,
+        Failed
     }
 }
